f6006a9d62ad4904c74f1c12295b506a75b29a0d
Yihui <0218tyh@gmail.com>
2023-05-08 01:18:57 -0400

    setup basic frontend test "pnpm test"

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,51 +1,54 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
-// import '@css/index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import '@css/index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
+
+
+// window.global ||= window;
 //
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-window.global ||= window;
-
-
-type State = {
-  name:string,
-  count:number
-}
-
-let state :State ={
-  name:"Doggr~",
-  count:0
-}
-
-global.changeName=()=>{
-  state.name = "Catte";
-  console.log("change name")
-  render(state);
-}
-
-global.changeCount=()=>{
-  state.count += 1;
-  render(state)
-}
-
-function renderApp(state){
-  return `<div>
-<p onclick="changeName()">HI from ${state.name}</p>
-<p onclick="changeCount()" > You have clicked ${state.count} times. </p>
-</div>`;
-
-}
-
-function render(state) {
-  
-  let html = renderApp(state);
-  console.log("rendering")
-  
-  document.body.innerHTML = html;
-}
-render(state);
+//
+// type State = {
+//   name:string,
+//   count:number
+// }
+//
+// let state :State ={
+//   name:"Doggr~",
+//   count:0
+// }
+//
+// global.changeName=()=>{
+//   state.name = "Catte";
+//   console.log("change name")
+//   render(state);
+// }
+//
+// global.changeCount=()=>{
+//   state.count += 1;
+//   render(state)
+// }
+//
+// function renderApp(state){
+//   return `<div>
+// <p onclick="changeName()">HI from ${state.name}</p>
+// <p onclick="changeCount()" > You have clicked ${state.count} times. </p>
+// </div>`;
+//
+// }
+//
+// function render(state) {
+//
+//   let html = renderApp(state);
+//   console.log("rendering")
+//
+//   document.body.innerHTML = html;
+// }
+// render(state);
--- frontend/test/App.test.tsx
+++ frontend/test/App.test.tsx
@@ -1,5 +1,44 @@
-import { screen } from "@testing-library/react";
+
 import { expect, test } from "vitest";
+
+// import dependencies
+import React from 'react'
+
+// import API mocking utilities from Mock Service Worker
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
+
+// import react-testing methods
+import {render, fireEvent, screen} from '@testing-library/react'
+
+// add custom jest matchers from jest-dom
+import '@testing-library/jest-dom'
+
+import {App} from "../src/App.js"
+
+
+test('click and counts', async () => {
+	// Arrange: rendering our page into a var
+	let testRender = render(<App  />)
+	// Act
+	fireEvent.click(screen.getByText('count is 0'))
+	// Assert
+	expect(screen.getByText('count is 1')).toBeVisible();
+})
+
+describe("Renders React components correctly",async ()=>{
+	it("should render the page correctly", async ()=>{
+
+		let testrender = render(<App />)
+		//setup
+		const h1 = await screen.queryByText("Vite + React");
+
+		//expectations
+		expect(h1).not.toBeNull();
+		expect(h1).toBeVisible();
+	})
+})
+
 
 test("Math.sqrt()", () => {
 	expect(Math.sqrt(4)).toBe(2);
